d52c075cacd1d9860b9ed27278ff53ef93990917
Kyle Goodale <KGoodale13@users.noreply.github.com>
2019-02-22 20:59:31 -0500

    [WIP] Local variable name substitution implmentation. Currently has a bug where it subs global function names

--- src/main/scala/com/scalup/Compactor.scala
+++ src/main/scala/com/scalup/Compactor.scala
@@ -1,14 +1,18 @@
 package com.scalup
 import Compactor.{CompactorConfig, intToVarName}
+import com.scalup.visitors.LocalVariableSubVisitor.LocalVarSubData
+import com.scalup.visitors.{LocalVariableSubVisitor, ReducerVisitor}
 
 import scala.annotation.tailrec
+import scala.collection.mutable
 
 object Compactor {
   case class CompactorConfig(
     replaceLocalVars: Boolean = true
   )
 
-  val reservedNames = Set("if", "while", "do", "end", "repeat", "until", "client", "server", "require", "util", "math", "debug")
+  val reservedNames =
+    Set("if", "while", "do", "end", "repeat", "until", "client", "server", "require", "util", "math", "debug", "G")
 
   val variableCharPool = ('a' to 'z') ++ ('A' to 'Z')
 
@@ -22,37 +26,35 @@ object Compactor {
     }
     genVar(index)
   }
-
-
 }
 
 class Compactor(config: CompactorConfig = CompactorConfig()) {
 
-//  class GlobalVarWalker extends ASTWalker[Null, List[String]]((L1, L2) => L1 ++ L2, List[String]()) {
-//    override
-//    def walkVariable(variable: Variable, passthrough: Null, global: Boolean): List[String] = {
-//
-//    }
-//  }
-
-
-  def compactBlock(block: Block): List[String] = {
-    val globalVarWalker = new ASTWalker[Null, List[String]]((L1, L2) => L1 ++ L2, List[String]()) {
-      override def walkVariable(variable: Variable, passthrough: Null, global: Boolean): List[String] = {
+  def compactBlock(block: Block): Block = {
+    val globalVarWalker = new ReducerVisitor[Null, List[String]]((L1, L2) => L1 ++ L2, List[String]()) {
+      override def visitVariable(variable: Variable, passthrough: Null, global: Boolean): List[String] = {
         if(global)
           variable match {
             case NamedVariable(name) => List(name)
-            case PrefixedVariable(prefix, name) => walkExpression(prefix, passthrough)
+            case PrefixedVariable(prefix, name) => visitExpression(prefix, passthrough)
             case IndexedVariable(prefix, expression) =>
-              walkExpression(prefix, passthrough) ++ walkExpression(expression, passthrough)
+              visitExpression(prefix, passthrough) ++ visitExpression(expression, passthrough)
           }
         else List[String]()
       }
     }
 
-    val globalVariables = globalVarWalker.walkBlock(block, null)
-    println(globalVariables)
-    globalVariables
+    val globalVariables: Set[String] = globalVarWalker.visitBlock(block, null).toSet
+
+    // TODO: Handle cases such as `local hook = hook` so we don't replace the global hook ref but we still update the local ref
+
+    val localVariableReplacementNames =
+      Iterator.from(0)
+        .map(intToVarName)
+        .filter(n => !Compactor.reservedNames.contains(n) && !globalVariables.contains(n))
+
+    val localVariableSubVisitor = new LocalVariableSubVisitor
+    localVariableSubVisitor.visitBlock(block, LocalVarSubData(localVariableReplacementNames, mutable.Map()))
   }
 
 }
---
+++ src/main/scala/com/scalup/visitors/LocalVariableSubVisitor.scala
@@ -0,0 +1,116 @@
+package com.scalup.visitors
+import com.scalup._
+import com.scalup.visitors.LocalVariableSubVisitor.LocalVarSubData
+
+import scala.collection.mutable
+
+
+// Walks the AST and replaces local variable names with ones from the supplied iterator. Used for minification or obfuscation
+
+object LocalVariableSubVisitor {
+	case class LocalVarSubData(var variableNames: Iterator[String], nameSubs: mutable.Map[String, String]) {
+		override def clone: LocalVarSubData = {
+			variableNames.duplicate match {
+				case (i1, i2) =>
+					this.variableNames = i1 // Reassign this objects variables names because we destroyed the og iterator by duplicating it
+					LocalVarSubData(i2, nameSubs.clone())
+			}
+		}
+
+		def subVarName(originalName: String): String = {
+			val newName = variableNames.next()
+			nameSubs(originalName) = newName
+			newName
+		}
+
+		// Returns the variables substituted name or the originally passed name if there is no existing substitution
+		def getSubNameOrOrig(varName: String): String =
+			nameSubs.getOrElse(varName, varName)
+
+	}
+}
+
+class LocalVariableSubVisitor extends TreeBuildVisitor[LocalVarSubData] {
+
+	override def visitVariable(variable: Variable, passthrough: LocalVarSubData, global: Boolean = false): Variable =
+		if(!global) {
+			variable match {
+				case NamedVariable(name) => NamedVariable(passthrough.subVarName(name))
+				case PrefixedVariable(prefix, name) => PrefixedVariable(visitPrefixExpression(prefix, passthrough), passthrough.subVarName(name))
+				case IndexedVariable(prefix, expression) =>
+					IndexedVariable(visitPrefixExpression(prefix, passthrough), visitExpression(expression, passthrough))
+			}
+		} else super.visitVariable(variable, passthrough)
+
+	override def visitFunctionCall(call: FunctionCall, passthrough: LocalVarSubData): FunctionCall =
+		call match {
+			case f: FunctionCallWithoutSelf => super.visitFunctionCall(f, passthrough)
+
+			case FunctionCallWithSelf(prefix, name, arguments) =>
+				FunctionCallWithSelf(
+					visitPrefixExpression(prefix, passthrough),
+					passthrough.getSubNameOrOrig(name),
+					arguments.map(visitExpression(_, passthrough))
+				)
+		}
+
+	override def visitExpression(expression: Expression, passthrough: LocalVarSubData): Expression = {
+		expression match {
+
+			case FunctionExpression(body) =>
+				FunctionExpression(
+					FunctionBody(
+						body.parameters.copy(parameters = body.parameters.parameters.map(p => Parameter(passthrough.subVarName(p.name)))),
+						visitBlock(body.body, passthrough)
+					)
+				)
+
+			case _ => super.visitExpression(expression, passthrough)
+		}
+	}
+
+	override def visitStatement(statement: Statement, passthrough: LocalVarSubData): Statement = {
+
+		statement match {
+
+			case LocalDeclaration(names, expressionList) =>
+				LocalDeclaration(names.map(passthrough.subVarName), expressionList.map(visitExpression(_, passthrough)))
+
+			case ForLoop(variableName, initialValue, upperBound, stepValue, block) =>
+				ForLoop(
+					passthrough.subVarName(variableName),
+					visitExpression(initialValue, passthrough),
+					visitExpression(upperBound, passthrough),
+					stepValue.map(visitExpression(_, passthrough)),
+					visitBlock(block, passthrough)
+				)
+
+			case ForEachLoop(variables, expressions, block) =>
+				ForEachLoop(
+					variables.map(passthrough.subVarName),
+					expressions.map(visitExpression(_, passthrough)),
+					visitBlock(block, passthrough)
+				)
+
+			case FunctionDefinition(name, body, local) =>
+				FunctionDefinition(
+					if(local) FunctionName(name.name.map(passthrough.subVarName), name.withSelf) else name,
+					FunctionBody(
+						body.parameters.copy(parameters = body.parameters.parameters.map(p => Parameter(passthrough.subVarName(p.name)))),
+						visitBlock(body.body, passthrough)
+					),
+					local
+				)
+
+			case _ => super.visitStatement(statement, passthrough)
+		}
+	}
+
+	override def visitStatements(statements: List[Statement], passthrough: LocalVarSubData): List[Statement] =
+		statements.map(s => visitStatement(s, passthrough))
+
+	override def visitBlock(block: Block, passthrough: LocalVarSubData): Block =
+		Block(visitStatements(block.statements, passthrough.clone))
+
+
+}
